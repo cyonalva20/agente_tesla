0f99059fafbeefffc0025c3be440d91b04604879
Anghelo Pintado <anghelopintadovalverde@gmail.com>
2026-07-07 22:44:37 -0500

    Prepare Heroku deployment

--- main.py
+++ main.py
@@ -432,4 +432,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
